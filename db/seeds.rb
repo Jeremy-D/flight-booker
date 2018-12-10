de4eaa40c2522febc10c7b68d21f0b910cf74c1f
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2018-12-09 16:13:11 -0800

    setup - seed db with Airport models and Flight models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+iata_codes = ['SFO', 'JFK', 'ATL', 'LAX', 'SJC', 'SAN', 'PHX', 'DEN', 'SEA', 'BOS', 'HNL', 'PDX']
+airports = []
+
+def seed_airports(code_array)
+	airports = []
+	code_array.each do |code|
+		airport = Airport.create(iata_code:code)
+		airports.push(airport)
+	end
+	return airports
+end
+
+def seed_flights(airports)
+	airports.each do |airport|
+		airports.each do |other_airport|
+			if airport.iata_code != other_airport.iata_code
+				#figure out seeding fake datetime and duration later
+				Flight.create(from_airport_id: airport.id, to_airport_id: other_airport.id)
+			end
+		end
+	end
+end
+
+all_airports = seed_airports(iata_codes)
+seed_flights(all_airports)
+
+
